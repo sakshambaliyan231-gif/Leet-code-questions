c7de477998ec09daf0034dd057605e361ca2ca95
sakshambaliyan231-gif <sakshambaliyan231@gmail.com>
2026-09-22 16:30:21 +0530

    Time: 0 ms (100.00%) | Memory: 28.7 MB (49.47%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int alice = 0;
+        int bob = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] < 10) {
+              
+                alice += nums[i];
+            }
+            else {
+                bob += nums[i];
+            }
+        }
+
+        return alice != bob;
+    }
+};
